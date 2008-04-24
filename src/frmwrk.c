a9a71438745156b00e4f345abdbe2c9122428cce
Thomas Staale Hauge <tsh@vmetro.no>
2008-04-24 09:57:15 -0700

    Added count function. Added/changed some memory freeing.

--- src/frmwrk.c
+++ src/frmwrk.c
@@ -14,13 +14,40 @@ int setup_inputs(struct vfi_dev *dev, struct vfi_source **src, struct gengetopt_
 {
 	struct vfi_source *h = malloc(sizeof(*h)+sizeof(void *));
 	*src = h;
-	if (src == NULL)
+	if (*src == NULL)
 		return -ENOMEM;
 
 	h->f = get_inputs;
 	h->d = dev;
 	h->h[0] = (void *)opts;
 	return 0;
+}
+
+
+static const int max_count = 100000;
+static int count = 0;
+
+int count_function(void **e, struct vfi_dev *dev, struct vfi_async_handle *ah, char *result)
+{
+	int bs = 0;
+	int i;
+	int sig = (unsigned int)e[1] ^ (unsigned int)count_function;
+	
+	if (sig & ~0xffff)
+		return 0;
+
+	printf("Transfer Count = %d%n", ++count, &bs);
+
+	if (count == max_count) {
+		printf("\n");
+		count = 0;
+		return 0;
+	}
+
+	for (; bs > 0; bs--)
+		printf("\b");
+
+	return 1;
 }
 
 int show_function(void **e, struct vfi_dev *dev, struct vfi_async_handle *ah, char *result)
@@ -103,18 +130,18 @@ void *source_thread(void *source)
 	struct vfi_source *src = (struct vfi_source *)source;
 	ah = vfi_alloc_async_handle(NULL);
 	while (vfi_get_cmd(src,&cmd)) {
-		free(vfi_set_async_handle(ah,NULL));
 		if (!vfi_find_pre_cmd(src->d, ah, &cmd)) {
 			do {
 				vfi_invoke_cmd(src->d,"%s%srequest(%p)\n",cmd,strstr(cmd, "?") ? ",":"?",ah);
 				vfi_wait_async_handle(ah,&result,(void *)&e);
-				printf("%s\n",result);
+				//printf("%s\n",result);
 			} while (vfi_invoke_closure(e,src->d,ah,result));
 		}
+		free(vfi_set_async_handle(ah,NULL));
 	}
 
 	free(result);
-	free(e);
+	vfi_free_async_handle(ah);
 
 	return 0;
 }
@@ -129,6 +156,7 @@ void *driver_thread(void *h)
 	struct vfi_dev *dev = (struct vfi_dev *)h;
 	while (!vfi_dev_done(dev))
 		vfi_post_async_handle(dev);
+	
 	return 0;
 }
 
@@ -136,6 +164,7 @@ int initialize_api_commands(struct vfi_dev *dev)
 {
 	vfi_register_func(dev,"show",show_function);
 	vfi_register_func(dev,"copy",copy_function);
+	vfi_register_func(dev,"count",count_function);
 
 	vfi_register_pre_cmd(dev,"pipe",pipe_pre_cmd);
 	vfi_register_pre_cmd(dev,"bind_create",bind_create_pre_cmd);
@@ -152,7 +181,9 @@ int main (int argc, char **argv)
 {
 	struct gengetopt_args_info opts;
 	struct vfi_dev *dev;
-	struct vfi_source *src;
+	struct vfi_source *file_src = NULL;
+	struct vfi_source *stdin_src = NULL;
+	struct vfi_source *input_src = NULL;
 
 	pthread_t driver_tid;
 
@@ -176,21 +207,21 @@ int main (int argc, char **argv)
 	initialize_api_commands(dev);
 
 	pthread_create(&driver_tid,0,driver_thread,(void *)dev);
-
+	
 	while (opts.file_given--) {
 		FILE *file = fopen(*opts.file_arg++,"r");
-		if (!vfi_setup_file(dev,&src,file))
-			ft = process_commands(&file_tid, src, &opts) == 0;
+		if (!vfi_setup_file(dev,&file_src,file))
+			ft = process_commands(&file_tid, file_src, &opts) == 0;
 	}
 
 	if (opts.inputs_num) {
-		if (!setup_inputs(dev,&src,&opts))
-			ot = process_commands(&opts_tid, src, &opts) == 0;
+		if (!setup_inputs(dev,&input_src,&opts))
+			ot = process_commands(&opts_tid, input_src, &opts) == 0;
 	}
 
 	if (opts.interactive_given) {
-		if (!vfi_setup_file(dev,&src,stdin))
-			ut = process_commands(&user_tid, src, &opts) == 0;
+		if (!vfi_setup_file(dev,&stdin_src,stdin))
+			ut = process_commands(&user_tid, stdin_src, &opts) == 0;
 	}
 
 	if (ft) pthread_join(file_tid,0);
@@ -198,9 +229,19 @@ int main (int argc, char **argv)
 	if (ut) pthread_join(user_tid,0);
 
 	vfi_set_dev_done(dev);
-
 	pthread_join(driver_tid,NULL);
+
+	if (file_src)
+		vfi_teardown_file(file_src);
+	if (stdin_src)
+		vfi_teardown_file(stdin_src);
+	if (input_src)
+		free(input_src);
+
 	vfi_close(dev);
+
+	// We're messing with the opts so don't free it.
+	//cmdline_parser_free(&opts);
 
 	return 0;
 }
