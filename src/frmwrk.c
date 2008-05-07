6a80ee73fbbaa3cfcfe9afaa3d93747f14355ede
Thomas Staale Hauge <tsh@vmetro.no>
2008-05-07 09:51:47 -0700

    Added performance test.

--- src/frmwrk.c
+++ src/frmwrk.c
@@ -1,8 +1,12 @@
 #include <vfi_api.h>
 #include <pthread.h>
+#include <time.h>
 #include <fw_cmdline.h>
 #include <vfi_frmwrk.h>
 #include <mcheck.h>
+
+#define MAX_COUNT 1000000
+static int count = 0;
 
 int get_inputs(void **s, char **command)
 {
@@ -24,29 +28,174 @@ int setup_inputs(struct vfi_dev *dev, struct vfi_source **src, struct gengetopt_
 	return 0;
 }
 
-
-static const int max_count = 100000;
-static int count = 0;
-
 int count_function(void **e, struct vfi_dev *dev, struct vfi_async_handle *ah, char *result)
 {
-	int bs = 0;
-	int i;
+	long res;
 	int sig = (unsigned int)e[1] ^ (unsigned int)count_function;
 	
-	if (sig & ~0xffff)
+	if (sig & ~0xffff) {
+		printf("Closure has errors. Stopping\n");
 		return 0;
+	}
 
-	printf("Transfer Count = %d%n", ++count, &bs);
+	/* Stop the loop on error  */
+	if (vfi_get_hex_arg(result, "result", &res) && res) {
+		printf("Failed: %s\n", result);
+		return 0;
+	}
 
-	if (count == max_count) {
+	printf("Transfer Count = %d", ++count);
+
+	if (count == MAX_COUNT) {
 		printf("\n");
 		count = 0;
 		return 0;
 	}
 
-	for (; bs > 0; bs--)
-		printf("\b");
+	printf("\r");
+	return 1;
+}
+
+int send_function(void **e, struct vfi_dev *dev, struct vfi_async_handle *ah, char *result)
+{
+	long res;
+	int sig = (unsigned int)e[1] ^ (unsigned int)count_function;
+	
+	if (sig & ~0xffff) {
+		printf("Closure has errors. Stopping\n");
+		return 0;
+	}
+
+	/* Stop the loop on error  */
+	if (vfi_get_hex_arg(result, "result", &res) && res) {
+		printf("Failed: %s\n", result);
+		return 0;
+	}
+
+	if (++count == MAX_COUNT) {
+		printf("Send done. Did %d transfers\n", count);
+		count = 0;
+		return 0;
+	}
+
+	return 1;
+}
+
+static void print_perf(struct timespec *start, struct timespec *stop, double nbytes, double incr)
+{
+    double            secs;
+    char              bytes_str[64];
+    double            bytes;
+    char              speed_str[64];
+    double            bytes_per_sec;
+    char              incr_str[64];
+    struct timespec   time;
+
+    time.tv_sec = stop->tv_sec - start->tv_sec;
+    time.tv_nsec = stop->tv_nsec - start->tv_nsec;
+
+    secs = ((double) time.tv_sec) +
+           ( (double)time.tv_nsec / (double)1000000000 );
+
+    /* Make the incr size more readable */
+    sprintf(incr_str, "B");
+    if (incr >= (double)1024*1024*1024) {
+	    sprintf(incr_str, "GiB");
+	    incr /= (double)1024 * 1024 * 1024;
+    }
+    else if (incr >= (double)1024 * 1024) {
+	    sprintf(incr_str, "MiB");
+	    incr /= (double)1024 * 1024;
+    }
+    else if (incr >= (double)1024) {
+	    sprintf(incr_str, "KiB");
+	    incr /= (double)1024;
+    }
+
+    /* Make the number of bytes more readable */
+    sprintf(bytes_str, "B");
+    bytes = nbytes;
+    if (nbytes >= (double)1000000000) {
+	    sprintf(bytes_str, "GB");
+	    bytes = nbytes/(double)1000000000;
+    }
+    else if (nbytes >= (double)1000000) {
+	    sprintf(bytes_str, "MB");
+	    bytes = nbytes/(double)1000000;
+    }
+    else if (nbytes >= (double)1000) {
+	    sprintf(bytes_str, "KB");
+	    bytes = nbytes/(double)1000;
+    }
+
+    /* Make the performance more readable */
+    sprintf(speed_str, "B/s");
+    bytes_per_sec = nbytes/secs;
+    if (nbytes/secs >= (double)1000000) {
+        sprintf(speed_str, "MB/s");
+        bytes_per_sec = nbytes/secs/(double)1000000;
+    }
+    else if (nbytes/secs >= (double)1000) {
+        sprintf(speed_str, "KB/s");
+        bytes_per_sec = nbytes/secs/(double)1000;
+    }
+
+    printf ("\n"
+            "----------------------------------\n"
+            "  Last increment : %2.2f %s\n"
+            "  Bytes in total : %2.2f %s\n"
+            "  Seconds used   : %2.2f\n"
+            "  Transfer rate  : %2.3f %s\n"
+            "----------------------------------\n",
+            incr, incr_str,
+            bytes, bytes_str,
+            secs,
+            bytes_per_sec, speed_str );
+}
+
+int perf_function(void **e, struct vfi_dev *dev, struct vfi_async_handle *ah, char *result)
+{
+	long res;
+	static struct timespec start;
+	static struct timespec stop;
+	static double nbytes;
+	int sig = (unsigned int)e[1] ^ (unsigned int)count_function;
+	struct vfi_map *map = (struct vfi_map *)e[2];
+	
+	if (sig & ~0xffff) {
+		printf("Closure has errors. Stopping\n");
+		return 0;
+	}
+
+	/* Stop the loop on error  */
+	if (!vfi_get_hex_arg(result, "result", &res) && res) {
+		printf("Failed: %s\n", result);
+		return 0;
+	}
+
+	switch (++count) {
+
+	case 1:
+		break;
+
+	case 2:
+		if (clock_gettime(CLOCK_REALTIME, &start))
+			perror("clock_gettime start failed");
+		nbytes = 0;
+		break;
+
+	case MAX_COUNT:
+		if (clock_gettime(CLOCK_REALTIME, &stop))
+			perror("clock_gettime stop failed");
+		nbytes += (double)map->extent; 
+		print_perf(&start, &stop, nbytes, (double)map->extent);
+		count = 0;
+		return 0;
+
+	default:
+		nbytes += map->extent; 
+		break;
+	}
 
 	return 1;
 }
@@ -127,15 +276,27 @@ void *source_thread(void *source)
 	char *cmd = NULL;
 	char *result = NULL;
 	void **e;
+	//long err;
 	struct vfi_async_handle *ah;
 	struct vfi_source *src = (struct vfi_source *)source;
 	ah = vfi_alloc_async_handle(NULL);
 	while (vfi_get_cmd(src,&cmd)) {
 		if (!vfi_find_pre_cmd(src->d, ah, &cmd)) {
 			do {
+				//err = 0; 
 				vfi_invoke_cmd(src->d,"%s%srequest(%p)\n",cmd,strstr(cmd, "?") ? ",":"?",ah);
 				vfi_wait_async_handle(ah,&result,(void *)&e);
-				//printf("%s\n",result);
+
+				/* Break the loop on error  */
+#if 0
+				err |= vfi_get_hex_arg(result, "result", &err);
+				if (err) {
+					errno = err;
+					printf("Failed: %s\n", result);
+					perror("Command failed");
+					break;
+				}
+#endif
 			} while (vfi_invoke_closure(e,src->d,ah,result));
 		}
 		free(vfi_set_async_handle(ah,NULL));
@@ -143,7 +304,6 @@ void *source_thread(void *source)
 
 	free(result);
 	vfi_free_async_handle(ah);
-
 	return 0;
 }
 
@@ -166,6 +326,8 @@ int initialize_api_commands(struct vfi_dev *dev)
 	vfi_register_func(dev,"show",show_function);
 	vfi_register_func(dev,"copy",copy_function);
 	vfi_register_func(dev,"count",count_function);
+	vfi_register_func(dev,"send",send_function);
+	vfi_register_func(dev,"perf",perf_function);
 
 	vfi_register_pre_cmd(dev,"pipe",pipe_pre_cmd);
 	vfi_register_pre_cmd(dev,"bind_create",bind_create_pre_cmd);
@@ -198,6 +360,7 @@ int main (int argc, char **argv)
 
 	int rc;
 
+	/* Simple memory alloc and free tracing */
 	mtrace();
 
 	cmdline_parser_init(&opts);
